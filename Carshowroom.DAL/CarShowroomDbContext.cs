1651274c2f0291cd38e97289fadca30ea0f4a5c9
Oleh Terentiev <dejavucool97gmail.com>
2021-09-29 16:38:19 +0300

    Create DbContext

---
+++ Carshowroom.DAL/CarShowroomDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CarShowroom.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carshowroom.DAL
+{
+    public class CarShowroomDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public CarShowroomDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>(entity => entity.HasIndex(client => client.Phone).IsUnique());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

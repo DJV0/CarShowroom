14d4c8550b3ccf03c12d61350487bc17a5e6fa54
Oleh Terentiev <dejavucool97gmail.com>
2021-09-29 17:36:44 +0300

    Create client service

---
+++ CarShowroom.BLL/Services/ClientService.cs
@@ -0,0 +1,45 @@
+﻿using Carshowroom.DAL;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly CarShowroomDbContext _context;
+        public ClientService(CarShowroomDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client Get(int id)
+        {
+            return _context.Clients.FirstOrDefault(client => client.Id == id);
+        }
+
+        public ICollection<Client> GetAll()
+        {
+            return _context.Clients.ToList();
+        }
+
+        public void Update(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

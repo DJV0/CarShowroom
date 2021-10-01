4c3e2a95a060eb244cf51dd813eb28b1633f38bd
Oleh Terentiev <dejavucool97gmail.com>
2021-10-01 14:27:11 +0300

    Create entity

--- CarShowroom.Models/Employee.cs
+++ CarShowroom.Models/Employee.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CarShowroom.Models
 {
-    public class Client
+    public class Employee
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
+        public string Position { get; set; }
     }
 }
---
+++ CarShowroom.Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CarShowroom.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime BeginningOfWork { get; set; }
+        public DateTime EndingOfWork { get; set; }
+    }
+}

6dc9df641c58cf14eb90b6c0ec2e3fb2c7247c74
Oleh Terentiev <dejavucool97gmail.com>
2021-10-06 12:43:31 +0300

    Create mapper profile

---
+++ CarShowroom.WebAPI/DTOs/Profiles/ClientProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CarShowroom.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.DTOs.Profiles
+{
+    public class ClientProfile : Profile
+    {
+        public ClientProfile()
+        {
+            CreateMap<Client, ClientDTO>();
+            CreateMap<ClientDTO, Client>();
+        }
+    }
+}

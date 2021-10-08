adfa0b3026fafec83c78860209db48db1b7cb9af
Oleh Terentiev <dejavucool97gmail.com>
2021-10-08 21:13:58 +0300

    Create car profile

---
+++ CarShowroom.WebAPI/Infrastructure/Profiles/CarProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarShowroom.Models.Entities;
+using CarShowroom.WebAPI.DTOs;
+
+namespace CarShowroom.WebAPI.Infrastructure.Profiles
+{
+    public class CarProfile : Profile
+    {
+        public CarProfile()
+        {
+            CreateMap<Car, CarDTO>();
+            CreateMap<CarDTO, Car>();
+        }
+    }
+}

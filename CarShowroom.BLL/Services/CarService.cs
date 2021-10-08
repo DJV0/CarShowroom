93037b7976ae985f7dca7f5dbf304c16d5647b49
Oleh Terentiev <dejavucool97gmail.com>
2021-10-08 21:09:59 +0300

    Create car service

---
+++ CarShowroom.BLL/Services/CarService.cs
@@ -0,0 +1,16 @@
+﻿using Carshowroom.DAL;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class CarService : Service<Car>, ICarService
+    {
+        public CarService(CarShowroomDbContext context) : base(context) { }
+    }
+}

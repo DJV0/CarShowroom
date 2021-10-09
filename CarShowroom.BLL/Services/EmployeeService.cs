f99794f7f5c1344871948101e8e38790069aeae6
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:41:53 +0300

    Create employee service. Override Get(id) generic service method

---
+++ CarShowroom.BLL/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using Carshowroom.DAL;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class EmployeeService: Service<Employee>, IEmployeeService
+    {
+        public EmployeeService(CarShowroomDbContext context) : base(context) { }
+        public override Employee Get(int id)
+        {
+            var employee = base.Get(id);
+            context.Entry(employee).Collection(e => e.OrderEmployees).Query()
+                .Include(oe => oe.Order).ThenInclude(o=>o.Car).Load();
+            return employee;
+        }
+    }
+}

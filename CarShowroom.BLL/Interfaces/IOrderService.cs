b629000447849cfc23e3a35399a500188e7e74f5
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:39:02 +0300

    Create order service interface

---
+++ CarShowroom.BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using CarShowroom.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Interfaces
+{
+    public interface IOrderService : IService<Order>
+    {
+    }
+}

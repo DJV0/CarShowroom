c4a746515fccb61e1d517445e7d7609d5f325f77
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:47:48 +0300

    Create employee profile for automapper

---
+++ CarShowroom.WebAPI/Infrastructure/Profiles/EmployeeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CarShowroom.Models.Entities;
+using CarShowroom.WebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.Infrastructure.Profiles
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee, EmployeeDTO>();
+            CreateMap<EmployeeDTO, Employee>();
+            CreateMap<Employee, EmployeeDetailsDTO>()
+                .ForMember(dest => dest.Orders, opt => opt.MapFrom(src => src.OrderEmployees.Select(oe=>oe.Order)));
+        }
+    }
+}

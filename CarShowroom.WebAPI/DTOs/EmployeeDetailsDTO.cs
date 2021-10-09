2ef993ea663b1b9e042cd7efba30ab9d82aa5a39
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:47:20 +0300

    Create employee and employee details DTOs

---
+++ CarShowroom.WebAPI/DTOs/EmployeeDetailsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.DTOs
+{
+    public class EmployeeDetailsDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+
+        public IEnumerable<OrderDTO> Orders { get; set; }
+    }
+}

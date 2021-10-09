12963b7a0789b877cad0dea7a77d9d2c997c0989
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:42:35 +0300

    Create order service. Override GetAll() and Get(id) generic service methods

---
+++ CarShowroom.BLL/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Carshowroom.DAL;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class OrderService : Service<Order>, IOrderService
+    {
+        public OrderService(CarShowroomDbContext context) : base(context) { }
+        public override IEnumerable<Order> GetAll()
+        {
+            var orderList = base.GetAll();
+            foreach (var order in orderList)
+            {
+                context.Entry(order).Reference(o => o.Car).Load();
+            }
+            return orderList;
+        }
+        public override Order Get(int id)
+        {
+            var order = base.Get(id);
+            context.Entry(order).Reference(o => o.Car).Load();
+            return order;
+        }
+    }
+}

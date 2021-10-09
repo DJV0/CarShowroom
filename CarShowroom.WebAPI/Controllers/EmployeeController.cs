0d8e98568b0ae50cade1107550c12a7e7ce6b614
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 00:49:30 +0300

    Create employee controller. Implement Get() and Get(id) methods

---
+++ CarShowroom.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.WebAPI.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+        public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+        {
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeDTO>> Get()
+        {
+            return Ok(_mapper.Map<IEnumerable<EmployeeDTO>>(_employeeService.GetAll()));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<EmployeeDetailsDTO> Get(int id)
+        {
+            return Ok(_mapper.Map<EmployeeDetailsDTO>(_employeeService.Get(id)));
+        }
+    }
+}

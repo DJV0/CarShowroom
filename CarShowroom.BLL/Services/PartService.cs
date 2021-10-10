45c1a63ef253d1c8f8d8e50b7fe107e3c9f3e9f7
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 14:04:26 +0300

    Create part service. Override Get(id) generic service method

---
+++ CarShowroom.BLL/Services/PartService.cs
@@ -0,0 +1,23 @@
+﻿using Carshowroom.DAL;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class PartService : Service<Part>, IPartService
+    {
+        public PartService(CarShowroomDbContext context) : base(context) { }
+        public override Part Get(int id)
+        {
+            var part = base.Get(id);
+            context.Entry(part).Collection(p => p.OrderParts).Query().Include(op => op.Order).Load();
+            return part;
+        }
+    }
+}

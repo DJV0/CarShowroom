b2482283d2ff3da13bfccf7ecacb4c2888b5e17e
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 14:05:49 +0300

    Create part controller. Implement CRUD methods

---
+++ CarShowroom.WebAPI/Controllers/PartController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Entities;
+using CarShowroom.WebAPI.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PartController : ControllerBase
+    {
+        private readonly IPartService _partService;
+        private readonly IMapper _mapper;
+        public PartController(IPartService partService, IMapper mapper)
+        {
+            _partService = partService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PartDTO>> Get()
+        {
+            return Ok(_mapper.Map<IEnumerable<PartDTO>>(_partService.GetAll()));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PartDetailsDTO> Get(int id)
+        {
+            return Ok(_mapper.Map<PartDetailsDTO>(_partService.Get(id)));
+        }
+
+        [HttpPost]
+        public ActionResult<PartDTO> Post([FromBody] PartDTO partDTO)
+        {
+            var part = _partService.Add(_mapper.Map<Part>(partDTO));
+            return CreatedAtAction(nameof(Get), new { id = part.Id }, _mapper.Map<PartDTO>(part));
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] PartDTO partDTO)
+        {
+            if (id != partDTO.Id) ModelState.AddModelError("id", "Entered id doen't match with entity id");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _partService.Update(_mapper.Map<Part>(partDTO));
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _partService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}

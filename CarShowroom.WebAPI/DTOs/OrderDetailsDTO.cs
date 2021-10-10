c5315f596039903c4866e02bbedcb04ed0711c9f
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 22:38:18 +0300

    Create order DTOs

---
+++ CarShowroom.WebAPI/DTOs/OrderDetailsDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.DTOs
+{
+    public class OrderDetailsDTO
+    {
+        public int Id { get; set; }
+        public DateTime BeginningOfWork { get; set; }
+        public DateTime EndingOfWork { get; set; }
+
+        public int? CarId { get; set; }
+        public CarDTO Car { get; set; }
+
+        public IEnumerable<EmployeeDTO> Employees { get; set; }
+        public IEnumerable<PartDTO> Parts { get; set; }
+    }
+}

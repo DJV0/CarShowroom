fb69ac1eeb81fcf9ff89827d748af0205514d67b
Oleh Terentiev <dejavucool97gmail.com>
2021-10-10 22:38:40 +0300

    Create order mapper profile

---
+++ CarShowroom.WebAPI/Infrastructure/Profiles/OrderProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarShowroom.Models.Entities;
+using CarShowroom.WebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.Infrastructure.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderDTO, Order>();
+            CreateMap<Order, OrderDetailsDTO>()
+                .ForMember(dest => dest.Employees, opt => opt.MapFrom(src => src.OrderEmployees.Select(oe => oe.Employee)))
+                .ForMember(dest => dest.Parts, opt => opt.MapFrom(src => src.OrderParts.Select(op => op.Part)));
+        }
+    }
+}

3680d81ca48df6effac510c5d957e5cb37a14d99
Oleh Terentiev <dejavucool97gmail.com>
2021-10-11 16:57:06 +0300

    Rename custom exception

---
+++ CarShowroom.BLL/Exceptions/NotFoundEntityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Exceptions
+{
+    public class NotFoundEntityException : Exception
+    {
+        public NotFoundEntityException() : base() { }
+        public NotFoundEntityException(string message) : base(message) { }
+    }
+}

c2d94a290ee6427453c288e84b4d3cc9123a309c
Oleh Terentiev <dejavucool97gmail.com>
2021-10-20 17:52:34 +0300

    Declare statistics service interface which contains two method: GetPercentageOfMenAndWomenInService and GetAverageMileageForPartReplacement

---
+++ CarShowroom.BLL/Interfaces/IStatisticsService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using CarShowroom.Models.Statistics;
+
+
+namespace CarShowroom.BLL.Interfaces
+{
+    public interface IStatisticsService
+    {
+        Task<MenAndWomenPercengate> GetPercentageOfMenAndWomenInService();
+        Task<int> GetAverageMileageForPartReplacement(string partName);
+    }
+}

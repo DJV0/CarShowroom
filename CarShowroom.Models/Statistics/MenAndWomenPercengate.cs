ffb0f889f4918d8afd942ba68567258c9d797e19
Oleh Terentiev <dejavucool97gmail.com>
2021-10-20 17:53:57 +0300

    Create class entity for statistics service method GetPercentageOfMenAndWomenInService

---
+++ CarShowroom.Models/Statistics/MenAndWomenPercengate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Models.Statistics
+{
+    public class MenAndWomenPercengate
+    {
+        public double MenPercentage { get; set; }
+        public double WomenPercentage { get; set; }
+    }
+}

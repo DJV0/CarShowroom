bc9dd87d772499f4e34a0ba2767c57b98e24c785
Oleh Terentiev <dejavucool97gmail.com>
2021-10-20 17:55:37 +0300

    Create statistics controller. Implement GetMenAndWomenPercengateInOrders method

---
+++ CarShowroom.WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,34 @@
+﻿using CarShowroom.BLL.Interfaces;
+using CarShowroom.Models.Statistics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsService _statisticsService;
+        private readonly ILogger<StatisticsController> _logger;
+        public StatisticsController(IStatisticsService statisticsService, ILogger<StatisticsController> logger)
+        {
+            _statisticsService = statisticsService;
+            _logger = logger;
+        }
+
+        [HttpGet("GetMenAndWomenPercengateInOrders")]
+        public async Task<ActionResult<MenAndWomenPercengate>> GetMenAndWomenPercengateInOrders()
+        {
+            _logger.LogInformation("Getting statistics of men and women percentage in orders");
+
+            return await _statisticsService.GetPercentageOfMenAndWomenInService();
+        }
+    }
+}

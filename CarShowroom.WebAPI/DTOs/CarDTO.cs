a802da2c32d383ea8e88eee93de25f27f54ac878
Oleh Terentiev <dejavucool97gmail.com>
2021-11-01 21:42:52 +0200

    Add Mileage property

--- CarShowroom.WebAPI/DTOs/CarDTO.cs
+++ CarShowroom.WebAPI/DTOs/CarDTO.cs
@@ -8,6 +8,7 @@ namespace CarShowroom.WebAPI.DTOs
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
+        public int Mileage { get; set; }
         public string BodyStyle { get; set; }
         public string Color { get; set; }
 

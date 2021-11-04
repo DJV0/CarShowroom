167da4d66cb735ed8fd8d04b89bdce3f776e362f
Oleh Terentiev <dejavucool97gmail.com>
2021-11-04 13:15:20 +0200

    Refactoring

--- CarShowroom.BLL/Interfaces/IStatisticsService.cs
+++ CarShowroom.BLL/Interfaces/IStatisticsService.cs
@@ -11,7 +11,7 @@ namespace CarShowroom.BLL.Interfaces
         Task<MenAndWomenPercengate> GetPercentageOfMenAndWomenInService();
         Task<int> GetAverageMileageForPartReplacement(string partName, string Make, string Model);
         Task<IEnumerable<Order>> GetEmployeeOrdersInProgress(string name, string lastName);
-        Task<IEnumerable<Order>> GetOrdersAmountInCurrentMonth();
+        Task<int> GetOrdersAmountInCurrentMonth();
         Task<string> GetTheMostPopularCarInService();
         Task<IEnumerable<Car>> GetCarInShowroomByMake(string make);
         Task<string> GetCarPartThatBreakDownMostOften(string make, string model);

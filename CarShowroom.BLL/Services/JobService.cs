817b7d51ff83f6e16caa3392ddadecc982e17643
Oleh Terentiev <dejavucool97gmail.com>
2021-11-04 13:17:10 +0200

    Create job service. Implement updating statistics method

---
+++ CarShowroom.BLL/Services/JobService.cs
@@ -0,0 +1,27 @@
+﻿using CarShowroom.BLL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.BLL.Services
+{
+    public class JobService : IJobService
+    {
+        private readonly IStatisticsService _statisticsService;
+        private readonly ILogger<JobService> _logger;
+        public JobService(IStatisticsService statisticsService, ILogger<JobService> logger)
+        {
+            _statisticsService = statisticsService;
+            _logger = logger;
+        }
+        public async Task UpdateStatisticsJob()
+        {
+            _logger.LogInformation("Updating statistics data.");
+            await _statisticsService.GetOrdersAmountInCurrentMonth();
+            await _statisticsService.GetTheMostPopularCarInService();
+        }
+    }
+}

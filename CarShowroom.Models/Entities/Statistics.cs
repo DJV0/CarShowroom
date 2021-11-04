a81eb9882a90598a908a6b0be78521f1fd741c2f
Oleh Terentiev <dejavucool97gmail.com>
2021-11-04 13:17:28 +0200

    Create statistics entity

---
+++ CarShowroom.Models/Entities/Statistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Models.Entities
+{
+    public class Statistics
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

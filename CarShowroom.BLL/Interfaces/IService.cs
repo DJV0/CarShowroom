aa28cd1114bcf5570fec5a48dcf08b351cb32360
Oleh Terentiev <dejavucool97gmail.com>
2021-11-23 19:48:01 +0200

    Rename method get by id

--- CarShowroom.BLL/Interfaces/IService.cs
+++ CarShowroom.BLL/Interfaces/IService.cs
@@ -9,7 +9,7 @@ namespace CarShowroom.BLL.Interfaces
     public interface IService<T> where T : class
     {
         Task<T> AddAsync(T entity);
-        Task<T> GetAsync(int id);
+        Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id);

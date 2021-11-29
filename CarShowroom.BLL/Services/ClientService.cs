3cca20321ca3fdba192df2d23a6f073d591a946f
Oleh Terentiev <dejavucool97gmail.com>
2021-11-29 21:52:59 +0200

    Override update method

--- CarShowroom.BLL/Services/ClientService.cs
+++ CarShowroom.BLL/Services/ClientService.cs
@@ -1,4 +1,5 @@
-﻿using Carshowroom.DAL;
+﻿using AutoMapper;
+using Carshowroom.DAL;
 using CarShowroom.BLL.Exceptions;
 using CarShowroom.BLL.Interfaces;
 using CarShowroom.Models.Entities;
@@ -13,7 +14,8 @@ namespace CarShowroom.BLL.Services
 {
     public class ClientService : Service<Client>, IClientService
     {
-        public ClientService(CarShowroomDbContext context) : base(context) { }
+        private readonly IMapper _mapper;
+        public ClientService(CarShowroomDbContext context, IMapper mapper) : base(context) => _mapper = mapper;
         public override async Task<IEnumerable<Client>> GetAllAsync()
         {
             return await context.Clients.Include(c => c.Cars).ToListAsync();
@@ -25,6 +27,30 @@ namespace CarShowroom.BLL.Services
                 .FirstOrDefaultAsync(c => c.Id == id);
             if (client == null) throw new ItemNotFoundException($"{typeof(Client).Name} item with id {id} not found.");
             return client;
+        }
+        public override async Task UpdateAsync(Client entity)
+        {
+            var dbClient = await GetByIdAsync(entity.Id);
+            var carsToDelete = dbClient.Cars.Select(c=>c.Id).Except(entity.Cars.Select(c=>c.Id));
+            var carsToAdd = entity.Cars.Select(c => c.Id).Except(dbClient.Cars.Select(c => c.Id));
+            if (carsToDelete.Any())
+            {
+                foreach (var carId in carsToDelete)
+                {
+                    var carToDelete = dbClient.Cars.First(c => c.Id == carId);
+                    dbClient.Cars.Remove(carToDelete);
+                }
+            }
+            if (carsToAdd.Any())
+            {
+                foreach (var carId in carsToAdd)
+                {
+                    var carToAdd = entity.Cars.First(c => c.Id == carId);
+                    dbClient.Cars.Add(carToAdd);
+                }
+            }
+            context.Entry(dbClient).CurrentValues.SetValues(entity);
+            await context.SaveChangesAsync();
         }
     }
 }

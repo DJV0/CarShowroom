639dcaeafc54a1d7e8213f6daf426666f17c4b94
Oleh Terentiev <dejavucool97gmail.com>
2021-11-29 21:53:23 +0200

    Add validation. Refactoring

--- CarShowroom.Client/DTOs/ClientDTO.cs
+++ CarShowroom.Client/DTOs/ClientDTO.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarShowroom.Client.DTOs
 {
     public class ClientDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName { get; set; }
         public Gender Gender { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
-        public IEnumerable<CarDTO> Cars { get; set; }
+        public ICollection<CarDTO> Cars { get; set; }
     }
 }

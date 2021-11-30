f8041bfd2d07231300679058fcfe802d549a4763
Oleh Terentiev <dejavucool97gmail.com>
2021-11-30 19:14:59 +0200

    Implement part service

---
+++ CarShowroom.Client/Services/PartService.cs
@@ -0,0 +1,43 @@
+﻿using CarShowroom.Client.DTOs;
+using CarShowroom.Client.Infrastructure.HttpClients.Interfaces;
+using CarShowroom.Client.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Client.Services
+{
+    public class PartService : IPartService
+    {
+        private readonly IPartClient _partClient;
+        public PartService(IPartClient partClient)
+        {
+            _partClient = partClient;
+        }
+        public async Task<bool> CreatePart(PartDTO part)
+        {
+            return await _partClient.Create(part);
+        }
+
+        public async Task<bool> DeletePart(int id)
+        {
+            return await _partClient.Delete(id);
+        }
+
+        public async Task<PartDTO> GetPartById(int id)
+        {
+            return await _partClient.GetById(id);
+        }
+
+        public async Task<IEnumerable<PartDTO>> GetParts()
+        {
+            return await _partClient.GetAll();
+        }
+
+        public async Task<bool> UpdatePart(int partId, PartDTO part)
+        {
+            return await _partClient.Update(partId, part);
+        }
+    }
+}

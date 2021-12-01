fc842c35a0650eac81f5e594326e9d813ef18ff3
Oleh Terentiev <dejavucool97gmail.com>
2021-12-01 20:05:47 +0200

    Create employee service

---
+++ CarShowroom.Client/Services/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using CarShowroom.Client.DTOs;
+using CarShowroom.Client.Infrastructure.HttpClients.Interfaces;
+using CarShowroom.Client.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Client.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeClient _employeeClient;
+        public EmployeeService(IEmployeeClient employeeClient)
+        {
+            _employeeClient = employeeClient;
+        }
+
+        public async Task<bool> CreateEmployee(EmployeeDTO employee)
+        {
+            return await _employeeClient.Create(employee);
+        }
+
+        public async Task<bool> DeleteEmployee(int id)
+        {
+            return await _employeeClient.Delete(id);
+        }
+
+        public async Task<EmployeeDTO> GetEmployeeById(int id)
+        {
+            return await _employeeClient.GetById(id);
+        }
+
+        public async Task<IEnumerable<EmployeeDTO>> GetEmployees()
+        {
+            return await _employeeClient.GetAll();
+        }
+
+        public async Task<bool> UpdateEmployee(int employeeId, EmployeeDTO employee)
+        {
+            return await _employeeClient.Update(employeeId, employee);
+        }
+    }
+}
---
+++ CarShowroom.Client/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using CarShowroom.Client.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Client.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeDTO>> GetEmployees();
+        Task<EmployeeDTO> GetEmployeeById(int id);
+        Task<bool> CreateEmployee(EmployeeDTO employee);
+        Task<bool> UpdateEmployee(int employeeId, EmployeeDTO employee);
+        Task<bool> DeleteEmployee(int id);
+    }
+}

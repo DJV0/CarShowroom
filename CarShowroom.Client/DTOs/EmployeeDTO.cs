68020a07f3b16b2dbbd4adeaf51fce6d4cc718d9
Oleh Terentiev <dejavucool97gmail.com>
2021-12-01 20:04:24 +0200

    Add validation

--- CarShowroom.Client/DTOs/EmployeeDTO.cs
+++ CarShowroom.Client/DTOs/EmployeeDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace CarShowroom.Client.DTOs
     public class EmployeeDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
+        [Required]
         public string Position { get; set; }
     }
 }
--- CarShowroom.Client/DTOs/OrderDTO.cs
+++ CarShowroom.Client/DTOs/OrderDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace CarShowroom.Client.DTOs
     public class OrderDTO
     {
         public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime BeginningOfWork { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime EndingOfWork { get; set; }
+        [Required]
         public int? CarId { get; set; }
         public CarDTO Car { get; set; }
         public ICollection<EmployeeDTO> Employees { get; set; }

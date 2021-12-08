35c481117af7ba976a8e3b1281663566635f085c
Oleh Terentiev <dejavucool97gmail.com>
2021-12-08 14:19:17 +0200

    Add Role property

--- CarShowroom.WebAPI/DTOs/UserForRegistrationDTO.cs
+++ CarShowroom.WebAPI/DTOs/UserForRegistrationDTO.cs
@@ -14,5 +14,7 @@ namespace CarShowroom.WebAPI.DTOs
         public string Password { get; set; }
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
